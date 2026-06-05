4684df9aa52c2d0389782ca7519f1629aa6d828e
Alexey Pozdnyakov <avp70ru@mail.ru>
2026-06-06 02:28:50 +0700

    Описание изменений

--- sys_linux.c
+++ sys_linux.c
@@ -8,6 +8,7 @@
  */
  
 #define _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
 #include <stdio.h>      // Для fopen, fread, vfprintf, FILE
 #include <string.h>     // Для strlen, memset, memcpy
 #include <stdarg.h>     // Для va_list, va_start
